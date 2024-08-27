148063d0f60df1b5a8b82c06599cdc2a944cc7ea
Frankly <franklinpoveda67@gmail.com>
2024-08-27 12:30:58 -0500

    Se agrega el Proyecto

---
+++ script.js
@@ -0,0 +1,38 @@
+function encriptar() {
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase
+        .replace(/e/img, "enter")
+        .replace(/o/img, "ober")
+        .replace(/i/img, "imes")
+        .replace(/a/img, "ai")
+        .replace(/u/img, "ufat");
+
+    document.getElementById("textoResultado").textContent = textoEncriptado;
+    document.getElementById("botonCopiar").style.display = "inherit";
+}
+
+function desencriptar() {
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase
+        .replace(/enter/img, "e")
+        .replace(/ober/img, "o")
+        .replace(/imes/img, "i")
+        .replace(/ai/img, "a")
+        .replace(/ufat/img, "u");
+
+    document.getElementById("textoResultado").textContent = textoEncriptado;
+}
+
+function copiar() {
+    var contenido = document.getElementById("textoResultado").textContent;
+    navigator.clipboard.writeText(contenido)
+        .then(() => alert("Texto copiado al portapapeles"))
+        .catch(err => console.error("Error al copiar el texto: ", err));
+}
+
+// Añadir los event listeners
+document.querySelector("#botonEncriptado").onclick = encriptar;
+document.querySelector("#botonDesencriptado").onclick = desencriptar;
+document.querySelector("#botonCopiar").onclick = copiar;
